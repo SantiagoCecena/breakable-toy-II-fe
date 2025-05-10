39fa7bda3c2de0242ae8ea89e507bd806e875802
Santiago Cecena <santiago.cecena@encora.com>
2025-05-10 02:09:51 -0700

    feat: create Topbar component

---
+++ src/components/Topbar.tsx
@@ -0,0 +1,10 @@
+
+function Topbar() {
+    return (
+        <div className="h-20 bg-spotify-gray border-b border-[#282828] flex items-center justify-between px-6">
+
+        </div >
+    )
+}
+
+export default Topbar
