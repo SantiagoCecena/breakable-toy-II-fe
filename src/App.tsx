9d0142f334fa73e4cabd35e562ded0f107a2a806
Santiago Cecena <santiago.cecena@encora.com>
2025-05-10 02:09:19 -0700

    feat: replace added chars to url when load and add sidebar and topbar UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { useEffect } from "react"
+import Sidebar from "./components/Sidebar"
+import Topbar from "./components/Topbar"
 
 function App() {
 
+	useEffect(() => {
+		if (window.location.hash === "#_=_") {
+			window.history.replaceState(null, "", window.location.pathname)
+		}
+	}, [])
+
 	return (
-		<>
-			<h1>Hello world</h1>
-		</>
+		<div className="flex h-screen bg-spotify-gray text-white">
+			<Sidebar />
+			<main className="flex-1 overflow-hidden">
+				<Topbar />
+			</main>
+		</div>
 	)
 }
 
