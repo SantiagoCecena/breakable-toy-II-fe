79b852b82213401d43c38ec31d83b05478234ebe
Santiago Cecena <santiago.cecena@encora.com>
2025-05-10 02:06:49 -0700

    feat: creare router and define principal routes

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router";
 import App from "../App";
+import Login from "../pages/Login";
 
 
 export const router = createBrowserRouter([
     {
-        path: "/",
+        path: "/login",
+        element: <Login />
+    },
+    {
+        path: "/home",
         element: <App />
     }
 ])
