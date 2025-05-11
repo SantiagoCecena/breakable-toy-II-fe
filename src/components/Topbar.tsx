b09316b4929f33643c649d1d2914772cdafc8ccc
Santiago Cecena <santiago.cecena@encora.com>
2025-05-11 00:54:53 -0700

    feat: add Topbar component the dropdown menu with logout button

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,8 +1,28 @@
+import { LogOut, UserIcon } from "lucide-react"
+import { Button } from "./ui/button"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu"
+import { Link } from "react-router"
 
 function Topbar() {
     return (
         <div className="h-20 bg-spotify-gray border-b border-[#282828] flex items-center justify-between px-6">
-
+            <div className="flex items-center gap-2 ml-auto">
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" size="icon" className="rounded-full bg-black/50">
+                            <UserIcon className="w-5 h-5" />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end" className="bg-[#282828] border-[#3e3e3e] text-white">
+                        <DropdownMenuItem className="focus:bg-[#3f3f3f] focus:text-white">
+                            <Link to="/login" className="flex items-center gap-2">
+                                <LogOut className="text-white" />
+                                Logout
+                            </Link>
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
         </div >
     )
 }
