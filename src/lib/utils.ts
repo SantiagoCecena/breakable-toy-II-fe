64ad3f11a072ac54a1f33c3ec1e6f07ec2d70fa0
Santiago Cecena <santiago.cecena@encora.com>
2025-05-12 01:16:58 -0700

    feat: add fromMsToMinutes and formatNumberWithCommas utility functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export function fromMsToMinuts(ms: number | string) {
+  const minutes = Math.floor(Number(ms) / 60000)
+  const seconds = Math.floor((Number(ms) % 60000) / 1000)
+  return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`
+}
+
+export function formatNumberWithCommas(number: number | string) {
+  const num = Number(number)
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+}
