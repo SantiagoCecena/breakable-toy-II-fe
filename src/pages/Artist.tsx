dcb7955a83ace0c79b21fc1d7c5410fc92ed67c1
Santiago Cecena <santiago.cecena@encora.com>
2025-05-13 17:31:50 -0700

    feat: add loader for fetching an artist information

--- src/pages/Artist.tsx
+++ src/pages/Artist.tsx
@@ -2,22 +2,42 @@ import { ScrollArea } from "../components/ui/scroll-area";
 import ArtistHeader from "../components/Artist/ArtistHeader";
 import ArtistTopTracks from "../components/Artist/ArtistTopTracks";
 import ArtistAlbums from "../components/Artist/ArtistAlbums";
-import { artistAlbums, mockArtist, topTracks } from "../mocks/artist";
+import { useLoaderData, type LoaderFunctionArgs } from "react-router";
+import { spotify_instance } from "../lib/api";
 
 function Artist() {
+
+    const { artist, topTracks, albums } = useLoaderData();
     return (
         <div className="flex-1 overflow-hidden">
             <ScrollArea className="h-full">
                 <div>
-                    <ArtistHeader artist={mockArtist} />
+                    <ArtistHeader artist={artist} />
                     <div className="px-6 py-4">
                         <ArtistTopTracks topTracks={topTracks} />
-                        <ArtistAlbums albums={artistAlbums} />
+                        <ArtistAlbums albums={albums} />
                     </div>
                 </div>
             </ScrollArea>
         </div>
     )
+}
+
+export async function getArtistLoader({ request }: LoaderFunctionArgs) {
+    const artistId = request.url.split("/").pop();
+    try {
+        const response = await spotify_instance.get("/artists/" + artistId);
+        return {
+            artist: response.data.artist,
+            topTracks: response.data.top_tracks.tracks,
+            albums: response.data.artist_albums.items
+        };
+
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+
 }
 
 export default Artist;
