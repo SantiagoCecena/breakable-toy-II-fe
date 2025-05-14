8ea78ccb0428330328556fb663e9548db7952540
Santiago Cecena <santiago.cecena@encora.com>
2025-05-13 17:23:51 -0700

    feat: create axios instance and add interceptors for request and response

---
+++ src/lib/api.ts
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+export const auth_instance = axios.create({
+    baseURL: "http://127.0.0.1:8080/api/auth",
+
+})
+
+export const spotify_instance = axios.create({
+    baseURL: "http://127.0.0.1:8080/api/spotify"
+})
+
+spotify_instance.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("access_token");
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    }, (error) => Promise.reject(error)
+)
+
+spotify_instance.interceptors.response.use(null, (error) => {
+    if (error.response) {
+        if (error.response.status === 401) {
+            localStorage.removeItem("access_token");
+            window.location.href = "/login";
+        }
+    }
+    return Promise.reject(error)
+})
