fc60bfd1ea7a0ca1c8969c3c88dc0afbee3945fc
Santiago Cecena <santiago.cecena@encora.com>
2025-05-14 17:47:10 -0700

    feat: add Search page and errorElement for handling errors

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,9 +1,10 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, useRouteError } from "react-router";
 import App, { AppLoader } from "../App";
 import Login from "../pages/Login";
 import MainDashboard, { getTopTenArtistLoader } from "../pages/MainDashboard";
 import Artist, { getArtistLoader } from "../pages/Artist";
 import Album, { getAlbumLoader } from "../pages/Album";
+import Search, { searchLoader } from "../pages/Search";
 
 export const router = createBrowserRouter([
     {
@@ -13,11 +14,20 @@ export const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
+        errorElement: <LayoutErrorWrapper />,
         loader: AppLoader,
         children: [
             { index: true, element: <MainDashboard />, loader: getTopTenArtistLoader },
             { path: "artist/:id", element: <Artist />, loader: getArtistLoader },
-            { path: "album/:id", element: <Album />, loader: getAlbumLoader }
+            { path: "album/:id", element: <Album />, loader: getAlbumLoader },
+            { path: "search", element: <Search />, loader: searchLoader }
         ]
     }
 ])
+
+function LayoutErrorWrapper() {
+
+    const error = useRouteError();
+
+    return <App error={error} />
+}
