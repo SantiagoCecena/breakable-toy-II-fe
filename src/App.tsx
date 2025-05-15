15684b8052acd0e1e058b198a67e912ba8641d61
Santiago Cecena <santiago.cecena@encora.com>
2025-05-14 17:47:32 -0700

    feat: implement error handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import Sidebar from "./components/Sidebar"
 import Topbar from "./components/Topbar"
 import { Outlet, redirect, type LoaderFunctionArgs } from "react-router"
+import ErrorContent from "./components/ErrorContent"
 
-function App() {
+function App({ error }: { error?: unknown }) {
 
 	return (
 		<div className="flex h-screen bg-spotify-gray text-white">
 			<Sidebar />
 			<main className="flex-1 flex flex-col overflow-hidden">
 				<Topbar />
-				<Outlet />
+				{
+					error
+						? <ErrorContent error={error} />
+						: <Outlet />
+				}
 			</main>
 		</div>
 	)
